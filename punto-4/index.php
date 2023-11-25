eac826711f0bdb44497de9ef56e5863da398004c
miguelmoya20 <miguelfoto2023@gmail.com>
2023-11-25 17:12:13 -0500

    Punto 04 Finalizado

---
+++ punto-4/index.php
@@ -0,0 +1,14 @@
+<?php
+$name = " ";
+$lastName = " ";
+
+// Datos de entrada
+echo "Ingrese su nombre: ";
+$name = readline();
+
+echo "Ingrese su apellido: ";
+$lastName = readline();
+
+// Resultado
+echo "Su Nombre Completo es : " . $name . " " . $lastName;
+?>
