6d7c85c5dc1cf1a0d4206466868e8f56fb9fc683
miguelmoya20 <miguelfoto2023@gmail.com>
2023-11-25 17:11:58 -0500

    Punto 03 Finalizado

---
+++ punto-3/index.php
@@ -0,0 +1,14 @@
+<?php
+// Datos de entrada
+echo "Ingrese el primer número: ";
+$firstNumber = readline();
+
+echo "Ingrese el segundo número: ";
+$secondNumber = readline();
+
+// Proceso
+$result = $firstNumber + $secondNumber;
+
+// Resultado
+echo $firstNumber . " + " . $secondNumber . " = " . $result;
+?>
